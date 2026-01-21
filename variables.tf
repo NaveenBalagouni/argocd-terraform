70ed6569d806c9d23d6525b568c0489801bb0f36
NaveenBalagouni <159781517+NaveenBalagouni@users.noreply.github.com>
2026-01-21 19:33:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "git_repo_url" {
 variable "git_branch" {
   description = "Git branch to clone (can be updated for upgrades)"
   type        = string
-  default     = "2025-09"
+  default     = "2025-11"
 }
 
 variable "kubeconfig_path" {
