a262740aace63da555b05fcde18b4305791c16be
NaveenBalagouni <159781517+NaveenBalagouni@users.noreply.github.com>
2026-01-22 17:11:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ resource "helm_release" "opsmx_ssd" {
   chart = "/tmp/enterprise-ssd/charts/ssd"
 
   values = [
-    file("/tmp/enterprise-ssd/charts/ssd/ssd-minimal-values.yaml")
+    "/tmp/enterprise-ssd/charts/ssd/ssd-minimal-values.yaml"
   ]
 
   # Deployment Settings
